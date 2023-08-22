93ac29acc46f7c7d5908640e752934e44572731b
lonelyjoeparker <joe@lonelyjoeparker.com>
2023-08-22 09:13:09 +0100

    added bits to set up test dirs, and boilerplate align/assemble

--- verify_installation.sh
+++ verify_installation.sh
@@ -17,11 +17,25 @@
 #   <to be decided>
 #
 
+# set up folders
+mkdir ../test_dir
+mkdir ../test_dir/raw_reads
+mkdir ../test_dir/assembly
+mkdir ../test_dir/kraken
+cd ../test_dir/raw_reads
+
 # get test data
-get https://s3.console.aws.amazon.com/s3/object/genomics-2023-test-data?region=eu-west-1&prefix=AMP551_14a1252d_289f5c8f_0.fast5
+wget https://s3.console.aws.amazon.com/s3/object/genomics-2023-test-data?region=eu-west-1&prefix=AMP551_14a1252d_289f5c8f_0.fast5
 
 # run guppy to basecall
+cd ..
+guppy_basecaller -i ./raw_reads -s basecalled_sup -c dna_r10.4_e8.1_sup.cfg # check for a newer pore model!
+
+# concatenate raw_reads
+cat ./raw_reads/*q > ./all-basecalled.fastq
 
 # run flye to assemble
-
+  flye --nano-hq ./all-basecalled.fastq --out-dir ./assembly --genome-size 7m --threads 4 --iterations 3 --meta  #40 threads for IRIDIS
 # run kraken to classify
+path_to_kraken_database=/media/nbicgenomics/seq_dbs/kraken2/standard/
+kraken2 --db $path_to_kraken_database --threads 4 --report ./kraken/kraken2.report --output ./kraken/kraken2.out ./all-basecalled.fastq
